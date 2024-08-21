2340d471253695c27cb34df0ea2fbe12ccd7cf98
hoppv <phamhopxd1@gmail.com>
2024-08-21 18:24:42 +0700

    update task, delete task

--- controllers/task.js
+++ controllers/task.js
@@ -8,7 +8,6 @@ export const addNewTask = async (req, res) => {
     title,
     description,
     status,
-    userId,
     priority,
   });
   if (error) {
@@ -74,5 +73,85 @@ export const getAllTasks = async (req, res) => {
   }
 };
 
-export const updateTask = async (req, res) => {};
-export const deleteTask = async (req, res) => {};
+export const updateTask = async (req, res) => {
+  const { title, description, status, userId, priority, _id } = await req.body;
+
+  const { error } = taskSchema.validate({
+    title,
+    description,
+    status,
+    priority,
+  });
+  if (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.details[0].message,
+    });
+  }
+
+  try {
+    const updateTask = await Task.findByIdAndUpdate(
+      { _id },
+      {
+        title,
+        description,
+        status,
+        priority,
+        userId,
+      },
+      { new: true }
+    );
+
+    if (updateTask) {
+      return res.status(200).json({
+        success: true,
+        message: "Task updated successfully",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Some error occurred! Please try again",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Some error occurred! Please try again",
+    });
+  }
+};
+
+export const deleteTask = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!id) {
+      return res.status(400).json({
+        success: false,
+        message: "Task id is required",
+      });
+    }
+
+    const deleteTask = await Task.findByIdAndDelete(id);
+    if (deleteTask) {
+      return res.status(200).json({
+        success: true,
+        message: "Task deleted successfully",
+      });
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Some error occurred! Please try again",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Some error occurred! Please try again",
+    });
+  }
+};
--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { addNewTask, getAllTasks } from "../controllers/task.js";
+import {
+  addNewTask,
+  deleteTask,
+  getAllTasks,
+  updateTask,
+} from "../controllers/task.js";
 
 const taskRouter = express.Router();
 
 taskRouter.post("/add-new-task", addNewTask);
 taskRouter.get("/get-all-tasks-by-user-id/:id", getAllTasks);
+taskRouter.delete("/delete-task/:id", deleteTask);
+taskRouter.put("/update-task", updateTask);
 
 export default taskRouter;
